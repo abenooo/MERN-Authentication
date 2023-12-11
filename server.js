2d4f59db52439a801b334f24f1508c924393d1a6
Abenezer K <abenezerkifle000@gmail.com>
2023-12-11 05:23:23 -0800

    Fix the server.js

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
-const connectDB = require('./config/db')
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 3000;
-connectDB();
-app.get("/", (request, response) => response.send("Hello coder"));
-app.listen(PORT, () => console.log(`server is running in the PORT 30000 ${PORT}`));
 
+connectDB();
+
+// initalize middelweare
+app.use(express.json({ extended: false }));
+
+app.use("/api/users", require('./routes/api/users'));
+// app.get("/", (request, response) => response.send("Hello coder"));
+app.listen(PORT, () =>
+  console.log(`server is running in the PORT  ${PORT}`)
+);
