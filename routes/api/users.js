5ff6783ae5e0c7334207f861fb0a0c714515ec04
Abenezer K <abenezerkifle000@gmail.com>
2023-12-16 00:30:46 -0800

    Add express-validator and validate the input

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,33 +3,51 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const User = require("../../models/User");
 
+// adding checker
+const { check, validationResult } = require("express-validator");
 router.get("/", (req, res) => res.send("User route"));
-router.post("/", async (req, res) => {
-  const { name, email, password } = req.body;
-
-  try {
-    let user = await User.findOne({ email });
-    if (user) {
-      return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+router.post(
+  "/",
+  [
+    check("name", "name is required").not().isEmpty(),
+    check("email", "valid email address is required").isEmail(),
+    check("password", "Password must have atlease 4 characters").isLength({
+      min: 4,
+    }),
+  ],
+  async (req, res) => {
+    const { name, email, password } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    // create the new user if there is no user with that email
-    user = new User({
-      name,
-      email,
-      password,
-    });
 
-    // encrypt the password and return the value
-    const salt = await bcrypt.genSalt();
-    user.password = await bcrypt.hash(password, salt);
-    // save the user
-    await user.save();
-    // return the resposen
-    res.json(req.body);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send("Server error")
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+      // create the new user if there is no user with that email
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      // encrypt the password and return the value
+      const salt = await bcrypt.genSalt();
+      user.password = await bcrypt.hash(password, salt);
+      // save the user
+      await user.save();
+      // return the resposen
+      res.json(req.body);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Server error");
+    }
   }
-});
+);
 
 module.exports = router;
