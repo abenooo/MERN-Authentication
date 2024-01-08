56d9c68eee0b60577e878a977a1fbe87d7152a89
Abenezer K <abenezerkifle000@gmail.com>
2024-01-08 01:05:09 -0800

    Fix the error for creating account

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,22 +3,30 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const User = require("../../models/User");
 
-router.get('/', (req,res) => res.send("User route"))
-router.post('/', async (req,res) =>{
-    const {name,email,password} = req.body
+router.get("/", (req, res) => res.send("User route"));
+router.post("/", async (req, res) => {
+  const { name, email, password } = req.body;
 
-    try {
-        let user = await User.findOne({email});
-        if(user){
-        return res.status(400).json({errors: [{msg: "User already exists"}]});
-}
-// create the new user if there is no user with that email
-user = new User({
-    name,
-    email,
-    password
-})
-    } catch (error) {
-        
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ errors: [{ msg: "User already exists" }] });
     }
-})
+    // create the new user if there is no user with that email
+    user = new User({
+      name,
+      email,
+      password,
+    });
+    const salt = await bcrypt.genSalt();
+    user.password = await bcrypt.hash(password, salt);
+    await user.save();
+    res.json(req.body);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server error");
+  }
+});
+
+
+module.exports = router;
