cc2e0f7c3ff16ee507a8c5d83af38c2692592a61
Abenezer K <abenezerkifle000@gmail.com>
2024-01-08 04:27:55 -0800

    Decode the token and check the validatio of the token

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,17 @@ const config = require("config");
 
 module.exports = function(req,res,next){
     const token = req.header("x-auth-token");
+    // 401 means not autorized
     if(!token){
         return res.status(401).json({msg:"Not authrozed!"})
+    }
+    // validate token
+
+    try {
+        const decoded = jwt.verify(token,config.get('jwtSecret'));
+        req.user =  decoded.user;
+        next();
+    } catch (error) {
+        res.status(401).json({msg: "Invalid token"})
     }
 }
