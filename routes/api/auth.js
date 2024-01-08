c16902030ce84e9e35e1727cb94c4d00550116f6
Abenezer K <abenezerkifle000@gmail.com>
2024-01-08 04:39:58 -0800

    Make the Get route with auth as middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,3 +5,14 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 const { check, validationResult } = require("express-validator");
+
+// Get route
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select(-password);
+    res.json(user);
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
