43a7197ea116cbc1608fbb4580e5826e4c5cc29c
Abenezer K <abenezerkifle000@gmail.com>
2024-01-08 04:23:44 -0800

    Add middle ware for authorization

---
+++ middleware/auth.js
@@ -0,0 +1,9 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = function(req,res,next){
+    const token = req.header("x-auth-token");
+    if(!token){
+        return res.status(401).json({msg:"Not authrozed!"})
+    }
+}
