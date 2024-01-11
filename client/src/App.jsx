4ea4800c94f1f8dba0c4d89e66e6ff7f2b9c478a
Abenezer K <abenezerkifle000@gmail.com>
2024-01-11 06:52:43 -0800

    Render the componet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/layout/Home";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
-      <Navbar />
-      <Home />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
