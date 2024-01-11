79f0203b8c1ea950b0cc0e2e62e35dceb70456e7
Abenezer K <abenezerkifle000@gmail.com>
2024-01-11 06:17:53 -0800

    Create home componet

---
+++ client/src/components/layout/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+const Home = () => {
+  return (
+    <section>
+      <div>
+        <div>
+          <h1>Professional Developemnt</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
+            minima rerum culpa vitae quos distinctio voluptatum iste accusantium
+            facere in sequi illum dignissimos consequuntur? Facere dicta nobis
+            sint nesciunt corrupti.
+          </p>
+
+          <div>
+            <a href="register.html">Register</a>
+            <a href="login.html">Login</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
