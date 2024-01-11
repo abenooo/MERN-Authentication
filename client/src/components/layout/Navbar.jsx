2c9bf8473cba5e418c0322891c1e2a2a15d2f2dd
Abenezer K <abenezerkifle000@gmail.com>
2024-01-11 06:17:34 -0800

    Create navbar componet

---
+++ client/src/components/layout/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>
+        <a href="index.html">Mern Auth</a>
+      </h1>
+      <ul>
+        <li>
+          <a href="register.html">Register</a>
+        </li>
+        <li>
+          <a href="login.html">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
