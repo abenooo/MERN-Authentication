a0028d62a7441532500f1cf0fd4ffc6b41364ef9
Abenezer K <abenezerkifle000@gmail.com>
2024-01-11 06:17:14 -0800

    Import navbar and home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/layout/Home";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>App File</div>
-    </>
+    <div>
+      <Navbar />
+      <Home />
+    </div>
   );
 }
 
