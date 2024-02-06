38241095b40bc4bbf153c04d0a72476213874797
getasew <getasewmequanint@cbe.com.et>
2024-02-05 22:38:21 -0800

    Add the username and password filed

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -9,9 +9,41 @@ const Login = () => {
   const { email, password } = formData;
   const onChange = (e) => ({
     ...formData,
-    [e.target.name]:e.target.value,
+    [e.target.name]: e.target.value,
   });
-  return <div>Login</div>;
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("logged");
+  };
+  return (
+    <>
+      <h1>
+      
+        <p>Log in to your account</p>
+      </h1>
+      <form onSubmit={(e) => onSubmit(e)}>
+        <div>
+          <input 
+          type="email"
+          placeholder="Please enter your email address"
+          name="email"
+          value={email}
+          onChange={(e) => onChange(e)}
+          required
+          />
+           <input 
+          type="password"
+          placeholder="Please enter your password"
+          name="password"
+          value={password}
+          onChange={(e) => onChange(e)}
+          minLength={4}
+          />
+        </div>
+
+      </form>
+    </>
+  );
 };
 
 export default Login;
